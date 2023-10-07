65e3d39c7a19ddd3b460bb0cab4822c5eef97a9d
harsha <bayyaramharsha@gmail.com>
2023-10-07 22:52:18 +0530

    cmpltd

---
+++ 12/index.js
@@ -0,0 +1,18 @@
+// function outest(){
+//     var c = 20;
+//     function outer(b){
+//         function inner(){
+//             console.log(a,b,c)
+//         }
+//         var a = 10;
+//         return inner;
+//     }
+//     var a = 1000;
+//     return outer;
+// }
+
+// var a  =100;
+// const inner = outest()("hello world");
+// inner()
+
+
---
+++ 14/index.js
@@ -0,0 +1,22 @@
+// setTimeout(function () {
+//   console.log("timer");
+// }, 5000);
+
+
+// x(function y() {
+//   console.log("y");
+
+// // let count =0;
+// // document.getElementById("clickMe")
+// // .addEventListener("click",function xyz(){
+// //     console.log("button clicked",++count);
+// // })
+
+// // function attachEventListener() {
+// //   let count = 0;
+// //   document.getElementById("clickMe").addEventListener("click", function xyz() {
+// //     console.log("button clicked", ++count);
+// //   });
+// // }
+
+// // attachEventListener();
